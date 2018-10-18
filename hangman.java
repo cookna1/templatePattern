975e81197513337dc852d582121cb7b474772ee3
Nathan Cook <cookna1@appstate.edu>
2018-10-18 13:51:37 -0400

    basic game and hangman classes

---
+++ hangman.java
@@ -0,0 +1,30 @@
+public class hangman extends Game {
+    public String gameSoFar;
+    public int turns;
+
+    public hangman() {
+        gameSoFar = "";
+        turns = 7;
+    }
+
+    protected void initialize() {
+        
+    }
+    public void loadContent() {
+        
+    }
+    public void userAction() {
+        
+    }
+
+    public void draw() {
+        
+    }
+    public boolean evaluateState() {
+        return true;
+    }
+    protected void report() {
+        
+    }
+}
+
